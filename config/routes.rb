9adc3e64b96767ea639dbe6a69c0acaaffb0d5b2
Raiyan Sayeed <raiyan.business@gmail.com>
2022-01-18 10:29:12 -0500

    updated navbar and new warehouse button

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :inventories
   resources :warehouses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "home#index"
+  root "warehouses#index"
 end
